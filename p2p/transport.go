eea00a0d1a7255aa5cde100ced8dfb5beef8988c
Himanshu Rai <himanshuraimau9@gmail.com>
2025-05-18 23:22:40 +0530

    add initial p2p package and define Peer and Transport interfaces

---
+++ p2p/transport.go
@@ -0,0 +1,15 @@
+package p2p
+
+
+// Peer is an interface that represent the remote node
+type Peer interface{
+
+}
+
+
+// Transport is anything that handles the communication
+// between the nodes in the network. This can be of any form
+// (TCP,UDP,webSockets....)
+type Transport interface{
+
+}
